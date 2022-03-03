3a69b95c3b1242ae1a30d3cfab8aa10d77ebd2c6
Gordon10 <xynergy2020@gmail.com>
2022-03-03 16:51:18 +1100

    update

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
-const puppeteer = require('puppeteer')
-const ObjectsToCsv = require('objects-to-csv')
+const puppeteer = require("puppeteer");
+const ObjectsToCsv = require("objects-to-csv");
 
 const sleep = (time) => {
-
-  return new Promise(resolve =>{
-    setTimeout(()=>{resolve()}, time)
-  })
-
-}
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, time);
+  });
+};
 
 const fetchData = async (URL) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
+  await page.goto(URL);
 
-  page.goto(URL)
+  await page.screenshot({
+    path: "./screenshots/pic1.png",
+  });
 
-  page.screenshot({
-    path : "./screenshots/pic1.png"
-  })
-
-  await page.close()
-  await browser.close()
-}
-
-
+  await page.close();
+  await browser.close();
+};
 
 const main = () => {
+  const url = "https://www.hkex.com.hk/eng/sorc/options/stock_options_search.aspx"
 
-const url = `https://www.hkex.com.hk/eng/sorc/options/stock_options_search.aspx`
+  fetchData(url);
+};
 
-fetchData(url)
-  
-}
-
-main()
+main();
