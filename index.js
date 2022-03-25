4d1a6f7d6a1b88e04cc3398f1fb5644cb5c236b9
Gordon10 <xynergy2020@gmail.com>
2022-03-25 17:05:36 +1100

    update

--- index.js
+++ index.js
@@ -15,8 +15,60 @@ const fetchData = async (URL) => {
 
   await page.goto(URL);
 
+  const firstBarHandlers = await page.$$("#slider-range > span");
+  const firstBarRange = await page.$("#slider-range > div");
+
+  await firstBarHandlers[0].evaluate((el) => (el.style.left = 0 + "%"));
+  await firstBarHandlers[1].evaluate((el) => (el.style.left = 100 + "%"));
+
+  await firstBarRange.evaluate((el) => {
+    el.style.left = 0 + "%";
+    el.style.width = 100 + "%";
+  });
+
+  const secondBarHandlers = await page.$$("#slider-range2 > span");
+  const secondBarRange = await page.$("#slider-range2 > div");
+
+  await secondBarHandlers[0].evaluate((el) => (el.style.left = 0 + "%"));
+  await secondBarHandlers[1].evaluate((el) => (el.style.left = 100 + "%"));
+
+  await secondBarRange.evaluate((el) => {
+    el.style.left = 0 + "%";
+    el.style.width = 100 + "%";
+  });
+
+  const searchBtn = await page.$("#btn_search");
+
+  await searchBtn.evaluate((btn) => btn.click());
+
+  await sleep(2000)
+
+  const rows = await page.$$(
+    "table.table.fixed-freeze-tb-container.alttable.stickysort-header-table.sticky-enabled > tbody > tr"
+    );
+  
+  console.log(rows.length)
+  
+  for (let row of rows) {
+
+    const data = await row.$(".btn_detail");
+
+    // const link = await data.evaluate((el) => el.getAttribute("href"));
+
+    // console.log(data);
+  }
+
+  // const form = await page.$('a#topbar-search');
+  // await form.evaluate( form => form.click() );
+  // await page.$eval( 'a#topbar-search', form => form.click() );
+
+  await page.setViewport({
+    width: 1200,
+    height: 1200,
+  });
+
   await page.screenshot({
-    path: "./screenshots/pic1.png",
+    path: "./screenshots/pic.png",
   });
 
   await page.close();
@@ -24,9 +76,14 @@ const fetchData = async (URL) => {
 };
 
 const main = () => {
-  const url = "https://www.hkex.com.hk/eng/sorc/options/stock_options_search.aspx"
+  const url =
+    "https://www.hkex.com.hk/eng/sorc/options/stock_options_search.aspx";
 
-  fetchData(url);
+  try {
+    fetchData(url);
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 main();
